a1de2b64bfc22f050040a925112d6895fe3676ae
Tobias Kaupat <tobiall@niondir.de>
2020-08-23 19:38:16 +0200

    Update decoder.js

--- wmbus-bridge/decoder.js
+++ wmbus-bridge/decoder.js
@@ -42,17 +42,26 @@ function Decoder(bytes, port) {
     // (array) of bytes to an object of fields.
     var decoded = {};
 
+    if (port === 9) {
+        decoded.devStatus = bytes[0];
+        decoded.devID = bytes[1] | bytes[2] << 8 | bytes[3] << 16 | bytes[4] << 24;
+        decoded.dif = bytes[5];
+        decoded.vif = bytes[6];
+        decoded.data0 = bytes[7];
+        decoded.data1 = bytes[8];
+        decoded.data2 = bytes[9];
+    }
+
     // example decoder for status packet by lobaro
-    if (port === 1 && bytes.length == 7) {
+    if (port === 1 && bytes.length == 9) { // status packet
+        decoded.FirmwareVersion = String.fromCharCode.apply(null, bytes.slice(0, 5)); // byte 0-4
+        decoded.Vbat = (bytes[5] | bytes[6] << 8) / 1000.0; // byte 6-7 (originally in mV)
+        decoded.Temp = (bytes[7] | bytes[8] << 8) / 10.0; // byte 8-9 (originally in 10th degree C)
+        decoded.msg = "Firmware Version: v" + decoded.FirmwareVersion + " Battery: " + decoded.Vbat + "V Temperature: " + decoded.Temp + "°C";
+    } else if (port === 1 && bytes.length == 7) {
         decoded.FirmwareVersion = readVersion(bytes, 0); // byte 0-2
         decoded.Vbat = (bytes[3] | bytes[4] << 8) / 1000.0; // originally in mV
-
-        var tempRaw16 = (bytes[4] | bytes[5] << 8);
-        if( (tempRaw16 & 1<<15) > 0){ // temp is negative (16bit 2's complement)
-          tempRaw16 = ((~tempRaw16)& 0xffff)+1; // invert 16bits & add 1 => now positive value 
-          tempRaw16=tempRaw16*-1;
-        }
-        decoded.Temp = tempRaw16 / 10.0; // originally in 10th degree C
+        decoded.Temp = (bytes[5] | bytes[6] << 8) / 10.0; // originally in 10th degree C
         decoded.msg = "Firmware Version: " + decoded.FirmwareVersion + " Battery: " + decoded.Vbat + "V Temperature: " + decoded.Temp + "°C";
     }
 
