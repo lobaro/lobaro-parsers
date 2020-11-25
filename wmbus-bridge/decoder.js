ae1b924dde79eacb78497c9b907b408d8f699a38
Tobias Kaupat <tobiall@niondir.de>
2020-11-25 11:44:27 +0100

    Update decoder.js

--- wmbus-bridge/decoder.js
+++ wmbus-bridge/decoder.js
@@ -1,62 +1,15 @@
-/**
- * TTN-compatible data decoder for the Lobaro LoRaWAN-wmbus-Bridge.
- *
- * For the current version of this and other data formats check out:
- * https://github.com/lobaro/ttn-data-formats/
- *
- * For more information go to:
- * https://www.lobaro.com/
- *
- *
- * MIT License
- *
- * Copyright (c) 2018 Lobaro
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
 function readVersion(bytes, i) {
     if (bytes.length < 3) {
         return null;
     }
     return "v" + bytes[i] + "." + bytes[i + 1] + "." + bytes[i + 2];
 }
-
-
-function signed(val, bits) {
-    if ((val & 1 << (bits - 1)) > 0) { // value is negative (16bit 2's complement)
-        var mask = Math.pow(2, bits) - 1;
-        val = (~val & mask) + 1; // invert all bits & add 1 => now positive value
-        val = val * -1;
-    }
-    return val;
-}
-
-function int16_LE(bytes, idx) {
-    bytes = bytes.slice(idx || 0);
-    return signed(bytes[1] << 8 | bytes[0] << 0, 16);
-}
-
+ 
 function Decoder(bytes, port) {
     // Decode an uplink message from a buffer
     // (array) of bytes to an object of fields.
     var decoded = {};
-
+ 
     if (port === 9) {
         decoded.devStatus = bytes[0];
         decoded.devID = bytes[1] | bytes[2] << 8 | bytes[3] << 16 | bytes[4] << 24;
@@ -66,19 +19,43 @@ function Decoder(bytes, port) {
         decoded.data1 = bytes[8];
         decoded.data2 = bytes[9];
     }
-
+ 
     // example decoder for status packet by lobaro
     if (port === 1 && bytes.length == 9) { // status packet
         decoded.FirmwareVersion = String.fromCharCode.apply(null, bytes.slice(0, 5)); // byte 0-4
         decoded.Vbat = (bytes[5] | bytes[6] << 8) / 1000.0; // byte 6-7 (originally in mV)
-        decoded.Temp = int16_LE(bytes, 7) / 10.0; // byte 8-9 (originally in 10th degree C)
+        decoded.Temp = (bytes[7] | bytes[8] << 8) / 10.0; // byte 8-9 (originally in 10th degree C)
         decoded.msg = "Firmware Version: v" + decoded.FirmwareVersion + " Battery: " + decoded.Vbat + "V Temperature: " + decoded.Temp + "°C";
     } else if (port === 1 && bytes.length == 7) {
         decoded.FirmwareVersion = readVersion(bytes, 0); // byte 0-2
         decoded.Vbat = (bytes[3] | bytes[4] << 8) / 1000.0; // originally in mV
-        decoded.Temp = int16_LE(bytes, 5) / 10.0; // originally in 10th degree C
+        decoded.Temp = (bytes[5] | bytes[6] << 8) / 10.0; // originally in 10th degree C
         decoded.msg = "Firmware Version: " + decoded.FirmwareVersion + " Battery: " + decoded.Vbat + "V Temperature: " + decoded.Temp + "°C";
     }
-
+ 
     return decoded;
 }
+ 
+// Wrapper for Lobaro Platform
+function Parse(input) {
+    // Decode an incoming message to an object of fields.
+    var b = bytes(atob(input.data));
+    var decoded = Decoder(b, input.fPort);
+  
+    return decoded;
+}
+  
+// Wrapper for Loraserver / ChirpStack
+function Decode(fPort, bytes) {
+    return Decoder(bytes, fPort);
+}
+  
+// Wrapper for Digimondo niota.io
+// Uncomment only when used in niota!
+/*
+module.exports = function (payload, meta) {
+    const port = meta.lora.fport;
+    const buf = Buffer.from(payload, 'hex');
+  
+    return Decoder(buf, port);
+}*/
